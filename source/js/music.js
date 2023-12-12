b51f24fda635a3f79e5071f1d354357d5cefaa3e
Jose wan <wleelw@sondy.eu.org>
2023-12-12 14:53:59 +0800

    Bug Repair
    删除冗余

--- source/js/music.js
+++ source/js/music.js
@@ -61,14 +61,11 @@ var scoMusic = {
             });
     },
     addEventListenerChangeMusicBg: function () {
-        const MusicPage = document.getElementById("Music-page");
-
-        MusicPage.querySelector("meting-js")
-            .aplayer.on('loadeddata', function () {
+        const aplayer = document.getElementById("Music-page").querySelector("meting-js").aplayer
+        aplayer.on('loadeddata', function () {
             scoMusic.changeMusicBg();
         });
-        MusicPage.querySelector("meting-js")
-            .aplayer.on('timeupdate', function () {
+        aplayer.on('timeupdate', function () {
             scoMusic.lrcupdate();
         });
         scoMusic.buttonlist();
@@ -101,25 +98,21 @@ var scoMusic = {
             }
             if (event.keyCode === 39) {
                 event.preventDefault();
-                document.querySelector('meting-js')
                 aplayer.skipForward();
             }
             if (event.keyCode === 37) {
                 event.preventDefault();
-                document.querySelector('meting-js')
                 aplayer.skipBack();
             }
             if (event.keyCode === 38) {
                 if (volume <= 1) {
                     volume += 0.1;
-                    document.querySelector('meting-js')
                     aplayer.volume(volume, true);
                 }
             }
             if (event.keyCode === 40) {
                 if (volume >= 0) {
                     volume += -0.1;
-                    document.querySelector('meting-js')
                     aplayer.volume(volume, true);
                 }
             }
