c29fe359e737292f01f02cef1af07714ba95037b
Jose wan <wleelw@sondy.eu.org>
2023-12-13 17:25:12 +0800

    Update
    新增匿名评论

--- source/js/main.js
+++ source/js/main.js
@@ -874,33 +874,25 @@ let sco = {
         }
     },
     addRandomCommentInfo: function () {
-        const e = `${adjectives[Math.floor(Math.random() * adjectives.length)]}${vegetablesAndFruits[Math.floor(Math.random() * vegetablesAndFruits.length)]}`;
-        !function () {
-            for (var t = ["#author", "input[name='comname']", "#inpName", "input[name='author']", "#ds-dialog-name", "#name", "input[name='nick']", "#comment_author"], o = ["#mail", "#email", "input[name='commail']", "#inpEmail", "input[name='email']", "#ds-dialog-email", "input[name='mail']", "#comment_email"], n = 0; n < t.length; n++) {
-                var a = document.querySelector(t[n]);
-                if (null != a) {
-                    a.value = e,
-                        a.dispatchEvent(new Event("input")),
-                        a.dispatchEvent(new Event("change"));
-                    break
-                }
-            }
-            for (var l = 0; l < o.length; l++) {
-                var i = document.querySelector(o[l]);
-                if (null != i) {
-                    i.value = "donotreply@examp.com",
-                        i.dispatchEvent(new Event("input")),
-                        i.dispatchEvent(new Event("change"));
-                    break
-                }
-            }
-        }();
-    },
-}
+        const e = `${GLOBAL_CONFIG.comment.randomInfoStart[Math.floor(Math.random() * GLOBAL_CONFIG.comment.randomInfoStart.length)]}${GLOBAL_CONFIG.comment.randomInfoEnd[Math.floor(Math.random() * GLOBAL_CONFIG.comment.randomInfoEnd.length)]}`;
 
-const adjectives = ["美丽的", "英俊的", "聪明的", "勇敢的", "可爱的", "慷慨的", "善良的", "可靠的", "开朗的", "成熟的", "稳重的", "真诚的", "幽默的", "豁达的", "有趣的", "活泼的", "优雅的", "敏捷的", "温柔的", "温暖的", "敬业的", "细心的", "耐心的", "深沉的", "朴素的", "含蓄的", "率直的", "开放的", "务实的", "坚强的", "自信的", "谦虚的", "文静的", "深刻的", "纯真的", "朝气蓬勃的", "慎重的", "大方的", "顽强的", "迷人的", "机智的", "善解人意的", "富有想象力的", "有魅力的", "独立的", "好奇的", "干净的", "宽容的", "尊重他人的", "体贴的", "守信的", "有耐性的", "有责任心的", "有担当的", "有远见的", "有智慧的", "有眼光的", "有冒险精神的", "有爱心的", "有同情心的", "喜欢思考的", "喜欢学习的", "具有批判性思维的", "善于表达的", "善于沟通的", "善于合作的", "善于领导的", "有激情的", "有幽默感的", "有思想的", "有个性的", "有正义感的", "有责任感的", "有创造力的", "有想象力的", "有艺术细胞的", "有团队精神的", "有协调能力的", "有决策能力的", "有组织能力的", "有学习能力的", "有执行能力的", "有分析能力的", "有逻辑思维的", "有创新能力的", "有专业素养的", "有商业头脑的"]
-    ,
-    vegetablesAndFruits = ["萝卜", "白菜", "芹菜", "生菜", "青椒", "辣椒", "茄子", "豆角", "黄瓜", "西红柿", "洋葱", "大蒜", "土豆", "南瓜", "豆腐", "韭菜", "花菜", "西兰花", "蘑菇", "金针菇", "苹果", "香蕉", "橙子", "柠檬", "猕猴桃", "草莓", "葡萄", "桃子", "杏子", "李子", "石榴", "西瓜", "哈密瓜", "蜜瓜", "樱桃", "蓝莓", "柿子", "橄榄", "柚子", "火龙果"];
+        const nameSelectors = ["#author", "input[name='comname']", "#inpName", "input[name='author']", "#ds-dialog-name", "#name", "input[name='nick']", "#comment_author"];
+        const emailSelectors = ["#mail", "#email", "input[name='commail']", "#inpEmail", "input[name='email']", "#ds-dialog-email", "input[name='mail']", "#comment_email"];
+
+        const nameElements = nameSelectors.map(selector => document.querySelector(selector)).filter(Boolean);
+        const emailElements = emailSelectors.map(selector => document.querySelector(selector)).filter(Boolean);
+
+        nameElements.forEach(element => {
+            element.value = e;
+            element.dispatchEvent(new Event("input"));
+        });
+
+        emailElements.forEach(element => {
+            element.value = "donotreply@examp.com";
+            element.dispatchEvent(new Event("input"));
+        });
+    }
+}
 
 /*
  * 代码高亮显示
