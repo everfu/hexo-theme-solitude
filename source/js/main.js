bc227d54f2d63f7df0ce0514a108a219449ba9ce
yife68 <2714344056@qq.com>
2023-12-21 15:43:27 +0800

    🐞 Bug Repair：首页文章标签跳转错误修复

--- source/js/main.js
+++ source/js/main.js
@@ -894,6 +894,9 @@ let sco = {
             element.value = "donotreply@examp.com";
             element.dispatchEvent(new Event("input"));
         });
+    },
+    aClickHandler: function(event) {
+        event.stopPropagation();
     }
 }
 
