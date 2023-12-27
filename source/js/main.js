5c788426fb6926844ef68bf5c8ad6e4a09586771
Jose wan <wleelw@sondy.eu.org>
2023-12-27 22:06:27 +0800

    🐛fix: 修复一些小bug
    ai、covercolor、music、header

--- source/js/main.js
+++ source/js/main.js
@@ -1029,11 +1029,6 @@ window.refreshFn = () => {
     GLOBAL_CONFIG.comment.commentBarrage && PAGE_CONFIG.comment && initializeCommentBarrage()
     document.body.setAttribute('data-type', PAGE_CONFIG.page)
     PAGE_CONFIG.page === "music" && scoMusic.init()
-    // Todo: 优化音乐播放器快捷键绑定
-    // if (!document.querySelector('#Music-page')) {
-    //     document.removeEventListener('keydown', scoMusic.setKeydown)
-    // }
-    // 暂时添加一个变量来判断是否在音乐页面
     GLOBAL_CONFIG.music.enable && !document.querySelector('#Music-page') && document.removeEventListener('keydown', scoMusic.setKeydown)
     GLOBAL_CONFIG.ai.enable && PAGE_CONFIG.page === "post" && ScoAI.init()
 }
