af422b1cd687194e6b7a108aea4147aae8b8f295
Jose wan <wleelw@sondy.eu.org>
2024-01-27 21:21:18 +0800

    ✨feat: repair covercolor

--- source/js/covercolor/api.js
+++ source/js/covercolor/api.js
@@ -2,12 +2,17 @@ const coverColor = () => {
     const path = document.getElementById("post-cover")?.src;
     if (path) {
         handleApiColor(path);
+    } else {
+        document.documentElement.style.setProperty('--sco-main', 'var(--sco-theme)');
+        document.documentElement.style.setProperty('--sco-main-op', 'var(--sco-theme-op)');
+        document.documentElement.style.setProperty('--sco-main-op-deep', 'var(--sco-theme-op-deep)');
+        document.documentElement.style.setProperty('--sco-main-none', 'var(--sco-theme-none)');
+        initThemeColor()
     }
 }
 
 function handleApiColor(path) {
     const cacheGroup = JSON.parse(localStorage.getItem('Solitude')) || {};
-    console.log(cacheGroup)
     if (cacheGroup.postcolor && cacheGroup.postcolor[path]) {
         const color = cacheGroup.postcolor[path].value;
         const [r, g, b] = color.match(/\w\w/g).map(x => parseInt(x, 16));
@@ -26,14 +31,13 @@ function img2color(src) {
             const [r, g, b] = color.match(/\w\w/g).map(x => parseInt(x, 16));
             setThemeColors(color, r, g, b);
             const expirationTime = Date.now() + coverColorConfig.time;
-            const cacheGroup = saveToLocal.get('Solitude') || {};
+            const cacheGroup = JSON.parse(localStorage.getItem('Solitude')) || {};
             cacheGroup.postcolor = cacheGroup.postcolor || {};
             cacheGroup.postcolor[src] = {value: color, expiration: expirationTime};
             localStorage.setItem('Solitude', JSON.stringify(cacheGroup));
         })
         .catch(error => {
             console.error('请检查API是否正常！\n' + error);
-            setThemeColors();
         });
 }
 
@@ -45,14 +49,14 @@ function setThemeColors(value, r = null, g = null, b = null) {
         document.documentElement.style.setProperty('--sco-main-none', value + '00');
 
         if (r && g && b) {
-            var brightness = Math.round(((parseInt(r) * 299) + (parseInt(g) * 587) + (parseInt(b) * 114)) / 1000);
+            let brightness = Math.round(((parseInt(r) * 299) + (parseInt(g) * 587) + (parseInt(b) * 114)) / 1000);
             if (brightness < 125) {
-                var cardContents = document.getElementsByClassName('card-content');
+                let cardContents = document.getElementsByClassName('card-content');
                 for (let i = 0; i < cardContents.length; i++) {
                     cardContents[i].style.setProperty('--sco-card-bg', 'var(--sco-white)');
                 }
 
-                var authorInfo = document.getElementsByClassName('author-info__sayhi');
+                let authorInfo = document.getElementsByClassName('author-info__sayhi');
                 for (let i = 0; i < authorInfo.length; i++) {
                     authorInfo[i].style.setProperty('background', 'var(--sco-white-op)');
                     authorInfo[i].style.setProperty('color', 'var(--sco-white)');
