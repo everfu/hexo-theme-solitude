7d30417b25d3a7d89e61a4c1e3eec09f15e5cda3
Efu <o@efu.me>
2024-03-23 20:46:06 +0800

    fix: #160

--- scripts/filter/default.js
+++ scripts/filter/default.js
@@ -6,19 +6,16 @@ hexo.extend.filter.register('after_post_render', function (data) {
     if (data.layout === 'post') {
         data.locate = data.locate || config.post.default.locate
         data.cc = data.cc || config.post.default.copyright
-        data.cover = data.cover || config.post.default.cover[getRandomInt(0, config.post.default.cover.length)] || '/img/default.png'
+        data.cover = data.cover || config.post.default.cover[getRandomInt(0, config.post.default?.cover?.length)] || '/img/default.png'
         data.excerpt = data.description || data.excerpt
-        if (config.aside.toc.post && data.toc !== false) data.toc = true
-        else data.toc = false
+        data.toc = !!(config.aside.toc.post && data.toc !== false);
     }
     if (data.layout === 'page') {
-        data.cover = data.cover || config.page.default.cover[getRandomInt(0, config.post.default.cover.length)] || '/img/default.png'
+        data.cover = data.cover || config.page.default.cover[getRandomInt(0, config.post.default?.cover?.length)] || '/img/default.png'
         data.excerpt = data.title
-        if (config.aside.toc.page && data.toc !== false && data.aside) data.toc = true
-        else data.toc = false
+        data.toc = !!(config.aside.toc.page && data.toc !== false && data.aside);
     }
-    if (config.comment.enable && data.comment !== false) data.comment = true
-    else data.comment = false
+    data.comment = !!(config.comment.enable && data.comment !== false);
     return data;
 });
 
