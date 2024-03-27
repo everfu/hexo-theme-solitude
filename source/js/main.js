e6552c96d3df8310cbd9cbfe2115e8917469d118
Efu <o@efu.me>
2024-03-27 15:30:12 +0800

    feat: 完善 i18

--- source/js/main.js
+++ source/js/main.js
@@ -824,7 +824,6 @@ window.refreshFn = () => {
     document.body.setAttribute('data-type', PAGE_CONFIG.page)
     PAGE_CONFIG.page === "music" && scoMusic.init()
     GLOBAL_CONFIG.ai.enable && PAGE_CONFIG.page === "post" && ScoAI.init()
-    preloader.removePaceDone()
 }
 
 document.addEventListener('DOMContentLoaded', function () {
