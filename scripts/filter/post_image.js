605e31172eafa7d7be9170deca05c7f37739f592
Ever Fu <o@efu.me>
2024-07-27 12:08:32 +0800

    chore: 删除注释

--- scripts/filter/post_image.js
+++ scripts/filter/post_image.js
@@ -4,7 +4,6 @@ hexo.extend.filter.register('before_post_render', data => {
   const imgTestReg = /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/i
   let { cover: coverVal } = data
 
-  // Add path to top_img and cover if post_asset_folder is enabled
   if (hexo.config.post_asset_folder) {
     if (coverVal && coverVal.indexOf('/') === -1 && imgTestReg.test(coverVal)) data.cover = `${data.path}${coverVal}`
   }
