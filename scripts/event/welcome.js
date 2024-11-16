ecf738ec52d93961433637655d381492768d6abe
一小只托尼 <o@efu.me>
2024-11-17 00:00:41 +0800

    feat(scripts): 更新欢迎信息样式
    - 添加主题 logo
    - 使用 ANSI 转义码给欢迎信息上色
    - 优化欢迎信息的显示格式

--- scripts/event/welcome.js
+++ scripts/event/welcome.js
@@ -1,14 +1,18 @@
 hexo.on('ready', () => {
-    const {version} = require('../../package.json')
-    hexo.log.info(`
-  =====================[ Hexo Theme - Solitude ]=====================
+    const { version } = require('../../package.json')
+    const logo = `
        #####  ####  #     #####  ###### #     # ######   ######
       #      #    # #       #      #    #     # #      # #
        ##### #    # #       #      #    #     # #      # ######
            # #    # #       #      #    #     # #      # #
       ######  ####  ##### #####    #      ###   ######   ######
-                            ${version}
-  ===================================================================
-  GitHub: https://github.com/everfu/hexo-theme-solitude
-  `)
+    `.replace(/#/g, '●')
+    const message = `
+  \x1b[38;5;45m=====================[ Hexo Theme - Solitude ]=====================\x1b[0m
+  \x1b[38;5;45m${logo}\x1b[0m
+                         \x1b[38;5;45m version: ${version}\x1b[0m
+  \x1b[38;5;45m===================================================================\x1b[0m
+         \x1b[38;5;45mGitHub: https://github.com/everfu/hexo-theme-solitude\x1b[0m
+  `
+    hexo.log.info(message)
 })
