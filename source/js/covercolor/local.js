6d6ce78e2d867ae4a42e4fad57f6bcd3524b8067
青稚 <sweetcandymini@foxmail.com>
2024-12-26 00:22:16 +0800

    去除多余空格
    Signed-off-by: 青稚 <sweetcandymini@foxmail.com>

--- source/js/covercolor/local.js
+++ source/js/covercolor/local.js
@@ -61,34 +61,23 @@ const setThemeColors = (value, r = null, g = null, b = null) => {
     initThemeColor();
 }
 
-function LightenDarkenColor(col, amt) {
-  
+function LightenDarkenColor(col, amt) { 
     var usePound = false;
-  
     if (col[0] == "#") {
         col = col.slice(1);
         usePound = true;
     }
- 
     var num = parseInt(col,16);
- 
     var r = (num >> 16) + amt;
- 
     if (r > 255) r = 255;
     else if  (r < 0) r = 0;
- 
     var b = ((num >> 8) & 0x00FF) + amt;
- 
     if (b > 255) b = 255;
     else if  (b < 0) b = 0;
- 
     var g = (num & 0x0000FF) + amt;
- 
     if (g > 255) g = 255;
     else if (g < 0) g = 0;
- 
     return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-  
 }
 
 const adjustCardStyles = () => {
